6c0f303f12dacccd876c18bbd923722946c22069
Marco Pontello <marcopon@gmail.com>
2017-02-26 03:42:10 +0100

    Added block 0 writing.

--- seqbox.py
+++ seqbox.py
@@ -18,6 +18,7 @@ import argparse
 import tempfile
 import binascii
 import random
+from functools import partial
 
 PROGRAM_VER = "0.01a"
 
@@ -59,6 +60,13 @@ def get_cmdline():
     res = parser.parse_args()
     return res
 
+def getsha256(filename):
+    with open(filename, mode='rb') as f:
+        d = hashlib.sha256()
+        for buf in iter(partial(f.read, 1024*1024), b''):
+            d.update(buf)
+    return d.digest()
+
 
 def main():
     sbxblocksize = 512
@@ -79,25 +87,44 @@ def main():
     chunksize = sbxblocksize - sbxhdrsize
 
     print("reading %s..." % filename)
+    filesize = os.path.getsize(filename)
+    sha256 = getsha256(filename)
     fin = open(filename, "rb")
     fout = open(sbxfilename, "wb")
 
     blocks = 0
+    #write block 0 / header
 
+    parent_uid = 0
+    buffer = parent_uid.to_bytes(4, byteorder='little')
+    buffer += filesize.to_bytes(8, byteorder='little')
+    buffer += (bytes(filename, "utf-8")+b"\x00"*255)[:255]
+    buffer += sha256
+    buffer += b'\x1A' * (chunksize - len(buffer)) # padding
+
+    crc = binascii.crc_hqx(buffer,0)
+    
+    blockhdr = (sbxhdr + crc.to_bytes(2,byteorder='little') +
+                blocks.to_bytes(4, byteorder='little') +
+                sbxflags)
+    fout.write(blockhdr+buffer)
+
+    
+    #write all other blocks
     while True:
         buffer = fin.read(chunksize)
         if len(buffer) < chunksize:
             if len(buffer) == 0:
                 break
-            buffer += b'\x1A' * (chunksize - len(buffer))
+            buffer += b'\x1A' * (chunksize - len(buffer)) # padding the last block
         blocks += 1
         #print(fin.tell(), " ",end = "\r")
 
         crc = binascii.crc_hqx(buffer,0)
         
         blockhdr = (sbxhdr + crc.to_bytes(2,byteorder='little') +
-                blocks.to_bytes(4, byteorder='little') +
-                sbxflags)
+                    blocks.to_bytes(4, byteorder='little') +
+                    sbxflags)
         fout.write(blockhdr+buffer)
                 
         
